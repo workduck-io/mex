700603c4d2ebd7b0847301f0e0a6dbdfb1716554
Shubham Mittal <shubham9910103545@gmail.com>
2021-09-21 23:36:52 +0530

    moved to correct DDB

--- src/main/kotlin/com/workduck/repositories/RepositoryImpl.kt
+++ src/main/kotlin/com/workduck/repositories/RepositoryImpl.kt
@@ -1,11 +1,12 @@
 package com.workduck.repositories
 
 import com.amazonaws.services.dynamodbv2.AbstractAmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.workduck.models.Entity
 import com.workduck.models.Identifier
 
 class RepositoryImpl<T: Entity>(
-    val amazonDynamoDB: AbstractAmazonDynamoDB
+    val amazonDynamoDB: AmazonDynamoDB
 ): Repository<T> {
     override fun get(identifier: Identifier): T {
         TODO("Not yet implemented")
