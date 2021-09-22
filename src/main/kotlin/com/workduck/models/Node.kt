eba1f6b73de4a95c05349e6990eaf1b1ccd0f7a4
Shubham Mittal <shubham9910103545@gmail.com>
2021-09-22 20:43:08 +0530

    added content

--- src/main/kotlin/com/workduck/models/Node.kt
+++ src/main/kotlin/com/workduck/models/Node.kt
@@ -1,12 +1,14 @@
 package com.workduck.models
 
 import com.workduck.utils.Helper
+import kotlin.streams.toList
 
 
-enum class NodeStatus{
+enum class NodeStatus {
     LINKED,
     UNLINKED
 }
+
 data class Node(
     val id: String = Helper.generateId("NODE"),
     val version: String,
@@ -14,7 +16,12 @@ data class Node(
     val nodeSchemaIdentifier: NodeSchemaIdentifier,
     val status: NodeStatus = NodeStatus.LINKED,
     val associatedProperties: Set<AssociatedProperty>,
+    val data: List<Element>,
     val createdAt: Long
-): Entity{
-    val updatedAt : Long = System.currentTimeMillis()
+) : Entity {
+    val updatedAt: Long = System.currentTimeMillis()
+
+    fun content(): String = data.parallelStream().map {
+            element -> element.content()
+    }.toList().joinToString("")
 }
