16051a784b4a87a300eec0ec336143bf5f101d15
Varun Garg <varungarg@Varuns-MacBook-Pro.local>
2021-09-27 16:58:31 +0530

    append optimized - hacky

--- src/main/kotlin/com/workduck/models/NodeDataConverter.kt
+++ src/main/kotlin/com/workduck/models/NodeDataConverter.kt
@@ -6,24 +6,30 @@ import com.fasterxml.jackson.module.kotlin.readValue
 
 
 
-class NodeDataConverter : DynamoDBTypeConverter<String, List<Element>> {
+class NodeDataConverter : DynamoDBTypeConverter<MutableList<String>, List<Element>> {
 
 	private val objectMapper = ObjectMapper()
 
-	override fun convert(n : List<Element>): String? {
-		return objectMapper.writeValueAsString(n)
-		//val itemDimensions: List<Element> = n as List<Element>
-//		var nodeData : String = ""
-//		for(e in n) {
-//			nodeData += String.format("id:%s x content:%s x type:%s x children: %s", e.getID(), e.content(), e.getElementType(), e.getChildren())
-//		}
-//		return nodeData
-
+	//override fun convert()
+	override fun convert(n : List<Element>): MutableList<String> {
+		val listOfData : MutableList<String> = mutableListOf()
+		for(element in n){
+			val e : String = objectMapper.writeValueAsString(element)
+			listOfData += e
+		}
+		return listOfData
+		//return objectMapper.writeValueAsString(n)
 
 	}
 
-	override fun unconvert(nodeData: String): List<Element> {
-		return objectMapper.readValue<List<Element>>(nodeData)
+	override fun unconvert(nodeData: MutableList<String>): MutableList<Element> {
+		val listOfElements : MutableList<Element> = mutableListOf()
+		for(string in nodeData){
+			val element : Element = objectMapper.readValue(string)
+			listOfElements += element
+		}
+		return listOfElements
+		//return objectMapper.readValue<List<Element>>(nodeData)
 
 	}
 
--- src/main/kotlin/com/workduck/repositories/RepositoryImpl.kt
+++ src/main/kotlin/com/workduck/repositories/RepositoryImpl.kt
@@ -4,9 +4,16 @@ package com.workduck.repositories
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
 import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec
+import com.amazonaws.services.dynamodbv2.model.AttributeValue
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.workduck.models.Element
 import com.workduck.models.Entity
 import com.workduck.models.Identifier
 import com.workduck.models.Node
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 
 class RepositoryImpl<T: Entity>(
@@ -42,6 +49,33 @@ class RepositoryImpl<T: Entity>(
         return t;
     }
 
+    override fun append(identifier: Identifier, tableName: String, elements : MutableList<Element>) {
+        val table = dynamoDB.getTable(tableName)
+
+        //val map: ValueMap = ValueMap().withList(":img",elements)
+        //val objectMapper = ObjectMapper()
+       // val map: Map<String, Any> = objectMapper.convertValue(elements, MutableMap::class.java)
+
+       // val valueMap = ValueMap.withList(":val1", Arrays.asList(elements))
+        val objectMapper = ObjectMapper()
+        val elementsInStringFormat : MutableList<String> = mutableListOf()
+        for(e in elements){
+            val entry : String = objectMapper.writeValueAsString(e)
+            elementsInStringFormat += entry
+        }
+
+        val expressionAttributeValues: MutableMap<String, Any> = HashMap()
+        expressionAttributeValues[":val1"] = elementsInStringFormat
+        expressionAttributeValues[":empty_list"] = mutableListOf<Element>()
+
+        //val map: List<Element>? = objectMapper.convertValue(elements, List<Element>::class.java)
+        val updateItemSpec : UpdateItemSpec = UpdateItemSpec().withPrimaryKey("PK", identifier.id)
+            .withUpdateExpression("set SK = list_append(if_not_exists(SK, :empty_list), :val1)")
+            .withValueMap(expressionAttributeValues)
+
+
+        table.updateItem(updateItemSpec)
+    }
 
 
 
@@ -75,4 +109,5 @@ class RepositoryImpl<T: Entity>(
 
 
 }
+
 
