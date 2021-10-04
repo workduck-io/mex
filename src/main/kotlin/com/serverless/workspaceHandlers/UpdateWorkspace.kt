f1da856020653e31169a0c7d3c1afaa513db1cad
Varun Garg <varungarg@Varuns-MacBook-Pro.local>
2021-10-04 15:35:12 +0530

    Add and test CRUD endpoints for Workspace

---
+++ src/main/kotlin/com/serverless/workspaceHandlers/UpdateWorkspace.kt
@@ -0,0 +1,30 @@
+package com.serverless.workspaceHandlers
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.serverless.ApiGatewayResponse
+import com.serverless.Response
+import com.workduck.service.WorkspaceService
+import org.apache.logging.log4j.LogManager
+import java.util.*
+
+
+class UpdateWorkspace:RequestHandler<Map<String, Any>, ApiGatewayResponse> {
+
+	override fun handleRequest(input:Map<String, Any>, context:Context): ApiGatewayResponse {
+
+		val json = input["body"] as String
+		println("BODY STARTS" + json + "BODY ENDS")
+		WorkspaceService().updateWorkspace(json)
+
+		val responseBody = Response("Go!!!!!! Serverless v1.x! Your Kotlin function executed successfully!", input)
+		return ApiGatewayResponse.build {
+			statusCode = 200
+			objectBody = responseBody
+			headers = Collections.singletonMap<String, String>("X-Powered-By", "AWS Lambda & serverless")
+		}
+	}
+	companion object {
+		private val LOG = LogManager.getLogger(UpdateWorkspace::class.java)
+	}
+}
