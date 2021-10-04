33ddd2c68e00e48a824b988ba87fbf87df70372d
Varun Garg <varungarg@Varuns-MacBook-Pro.local>
2021-10-04 19:30:52 +0530

    Add and test endpoints for UserIdentifierMappingRecords

---
+++ src/main/kotlin/com/serverless/userIdentifierMappingHandlers/GetUserRecords.kt
@@ -0,0 +1,31 @@
+package com.serverless.userIdentifierMappingHandlers
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.serverless.ApiGatewayResponse
+import com.serverless.Response
+import com.workduck.service.UserIdentifierMappingService
+import org.apache.logging.log4j.LogManager
+import java.util.*
+
+class GetUserRecords: RequestHandler<Map<String, Any>, ApiGatewayResponse> {
+
+	override fun handleRequest(input:Map<String, Any>, context: Context): ApiGatewayResponse {
+
+		val pathParameters = input["pathParameters"] as Map<*, *>?
+		val userID = pathParameters!!["userID"] as String
+		//val identifierID = pathParameters!!["identifierID"] as String
+
+		UserIdentifierMappingService().getUserRecords(userID)
+
+		val responseBody = Response("Go!!!!!! Serverless v1.x! Your Kotlin function executed successfully!", input)
+		return ApiGatewayResponse.build {
+			statusCode = 200
+			objectBody = responseBody
+			headers = Collections.singletonMap<String, String>("X-Powered-By", "AWS Lambda & serverless")
+		}
+	}
+	companion object {
+		private val LOG = LogManager.getLogger(GetUserRecords::class.java)
+	}
+}
