63e8071f975839412ce1350d3ec63661036eccba
rpPanda <ruddhi@workduck.io>
2021-10-19 17:10:48 +0530

    deployment changes: REST to HTTP

--- src/main/kotlin/com/serverless/nodeHandlers/GetNodesByWorkspaceStrategy.kt
+++ src/main/kotlin/com/serverless/nodeHandlers/GetNodesByWorkspaceStrategy.kt
@@ -8,7 +8,7 @@ class GetNodesByWorkspaceStrategy : NodeStrategy {
 	override fun apply(input: Map<String, Any>, nodeService: NodeService): ApiGatewayResponse {
 		val errorMessage = "Error getting users!"
 		val pathParameters = input["pathParameters"] as Map<*, *>?
-		val workspaceID = pathParameters!!["id"] as String
+		val workspaceID = pathParameters!!["workspaceId"] as String
 
 		val nodes : MutableList<String>? = nodeService.getAllNodesWithWorkspaceID(workspaceID)
 		return ApiResponseHelper.generateStandardResponse(nodes as Any?, errorMessage)
