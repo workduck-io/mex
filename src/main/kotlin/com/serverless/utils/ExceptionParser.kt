ea03ce30fbf208b2482c5240e9c34fe829bf926d
Shubham Mittal <shubham9910103545@gmail.com>
2021-11-30 18:07:49 +0530

    Random gyaan

--- src/main/kotlin/com/serverless/utils/ExceptionParser.kt
+++ src/main/kotlin/com/serverless/utils/ExceptionParser.kt
@@ -3,15 +3,30 @@ package com.serverless.utils
 import com.amazonaws.services.kms.model.InvalidArnException
 import com.serverless.ApiGatewayResponse
 import com.serverless.ApiResponseHelper
+import jdk.internal.util.Preconditions
 
+
+/**
+ * Application isa combination of standard errors plus customised behaviour
+ *
+ * We should always try to fit into standard errors
+ *
+ * Every standardized error should effectively map to standard http response code
+ *
+ * IllegalArgumentException -> 400
+ * IllegalAccessException -> 404
+ *
+ * //wrong behaviour
+ * class CE : Exception{}
+ *
+ * // if really needed do this
+ * class CE : IllegalArgumentException{}
+ *
+ */
 object ExceptionParser {
-
     fun exceptionHandler(e: Exception) : ApiGatewayResponse{
         return when(e){
-            is IllegalArgumentException -> {
-                ApiResponseHelper.generateStandardErrorResponse(e.message?:"Error performing action")
-            }
-
+            is IllegalArgumentException -> ApiResponseHelper.generateStandardErrorResponse(e.message?:"Error performing action", 400)
             else -> {
                 throw e
             }
@@ -19,3 +34,4 @@ object ExceptionParser {
         }
     }
 }
+
