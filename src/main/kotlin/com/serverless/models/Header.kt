99d604bb6798176c29b14927723b7e8ee10de25a
varungarg <varun.garg03@gmail.com>
2022-03-02 15:26:30 +0530

    Accept workspace header as workspace-id

--- src/main/kotlin/com/serverless/models/Header.kt
+++ src/main/kotlin/com/serverless/models/Header.kt
@@ -1,8 +1,11 @@
 package com.serverless.models
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Header(
+
+   @JsonProperty("workspace-id")
    val workspaceID : String
 )
