5c7ff342f8dfb90cc139e7eb185ff17bb42005f7
varungarg <varun.garg03@gmail.com>
2022-03-12 00:46:46 +0530

    Add workspaceIdentifier when creating relationship via trigger

--- src/main/kotlin/com/serverless/ddbStreamTriggers/workspaceUpdateTrigger/AddedToLeafNodeStrategy.kt
+++ src/main/kotlin/com/serverless/ddbStreamTriggers/workspaceUpdateTrigger/AddedToLeafNodeStrategy.kt
@@ -6,7 +6,7 @@ import com.workduck.utils.Helper.splitIgnoreEmpty
 import com.workduck.utils.NodeHelper.getCommonPrefixNodePath
 
 class AddedToLeafNodeStrategy : OperationPerformedStrategy {
-    override fun createRelationships(relationshipService: RelationshipService, newNodeHierarchy: List<String>, oldNodeHierarchy: List<String>, addedPath: List<String>, removedPath: List<String>) {
+    override fun createRelationships(relationshipService: RelationshipService, workspaceID: String, newNodeHierarchy: List<String>, oldNodeHierarchy: List<String>, addedPath: List<String>, removedPath: List<String>) {
         val commonPrefix = getCommonPrefixNodePath(removedPath[0], addedPath[0])
 
         /* take last node from overlapping path since that will be starting node of the first relationship to be created */
@@ -17,7 +17,7 @@ class AddedToLeafNodeStrategy : OperationPerformedStrategy {
         /* now we have a list [ node1, node1id, node2, node2id, node3, node3id.. ], we take out ids and make pairs */
         val nodePairListForRelationship = relationshipList.filterIndexed { index, _ -> index % 2 != 0 }.toList().zipWithNext()
 
-        makeNodePairsAndCreateRelationships(relationshipService, nodePairListForRelationship)
+        makeNodePairsAndCreateRelationships(relationshipService, workspaceID, nodePairListForRelationship)
 
     }
 }
