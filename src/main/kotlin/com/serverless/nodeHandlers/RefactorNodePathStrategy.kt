8b1af4126dd5c9da527f23de05aebf958c3edfdc
varungarg <varun.garg03@gmail.com>
2022-03-30 20:27:38 +0530

    More cleanup

--- src/main/kotlin/com/serverless/nodeHandlers/RefactorNodePathStrategy.kt
+++ src/main/kotlin/com/serverless/nodeHandlers/RefactorNodePathStrategy.kt
@@ -3,14 +3,20 @@ package com.serverless.nodeHandlers
 import com.serverless.ApiGatewayResponse
 import com.serverless.ApiResponseHelper
 import com.serverless.models.Input
+import com.workduck.models.Workspace
 import com.workduck.service.NodeService
+import com.workduck.service.WorkspaceService
 
 class RefactorNodePathStrategy : NodeStrategy {
     override fun apply(input: Input, nodeService: NodeService): ApiGatewayResponse {
         val errorMessage = "Error updating node path"
 
+        val workspace = WorkspaceService().getWorkspace(input.headers.workspaceID) as Workspace?
+                ?: throw IllegalArgumentException("Invalid Workspace ID")
+
+
         return input.payload?.let{ request ->
-            ApiResponseHelper.generateStandardResponse(nodeService.refactor(request, input.headers.workspaceID)
+            ApiResponseHelper.generateStandardResponse(nodeService.refactor(request, workspace)
                     , errorMessage)} ?: ApiResponseHelper.generateStandardErrorResponse(errorMessage)
 
     }
