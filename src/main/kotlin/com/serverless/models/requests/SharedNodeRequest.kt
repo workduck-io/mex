d73d696f3498712ecf846347b4199bc09d0ad04d
varungarg <varun.garg03@gmail.com>
2022-05-05 15:38:39 +0530

    Add jsonIgnore to SharedNodeRequest dto

--- src/main/kotlin/com/serverless/models/requests/SharedNodeRequest.kt
+++ src/main/kotlin/com/serverless/models/requests/SharedNodeRequest.kt
@@ -1,11 +1,15 @@
 package com.serverless.models.requests
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonTypeName
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.serverless.utils.isMalformed
 import com.workduck.converters.AccessTypeDeserializer
 import com.workduck.models.AccessType
 import com.workduck.models.ItemType
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonTypeName("SharedNodeRequest")
 data class SharedNodeRequest(
 
     val nodeID: String,
