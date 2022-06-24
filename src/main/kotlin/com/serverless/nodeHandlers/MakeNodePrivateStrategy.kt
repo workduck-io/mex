2d44ed21a3e11da80ed924a97aeb97fb591a81d5
Varun Garg <varun.garg03@gmail.com>
2022-06-24 23:04:51 +0530

    Change status codes for makeNodePublic/Prrivate endpoints (#75)
    https://github.com/workduck-io/mex/pull/75

--- src/main/kotlin/com/serverless/nodeHandlers/MakeNodePrivateStrategy.kt
+++ src/main/kotlin/com/serverless/nodeHandlers/MakeNodePrivateStrategy.kt
@@ -8,14 +8,9 @@ import com.workduck.service.NodeService
 
 class MakeNodePrivateStrategy : NodeStrategy {
     override fun apply(input: Input, nodeService: NodeService): ApiGatewayResponse {
-        val nodeID = input.pathParameters?.id
-
-        return if(nodeID != null) {
-            nodeService.makeNodePrivate(nodeID, input.headers.workspaceID)
-            ApiResponseHelper.generateStandardResponse(nodeID, Messages.ERROR_MAKING_NODE_PRIVATE)
-        }
-        else{
-            ApiResponseHelper.generateStandardErrorResponse(Messages.ERROR_MAKING_NODE_PRIVATE)
-        }
+        return input.pathParameters?.id?.let {
+            nodeService.makeNodePrivate(it, input.headers.workspaceID)
+            ApiResponseHelper.generateStandardResponse(null, 204, Messages.ERROR_MAKING_NODE_PRIVATE)
+        }!!
     }
 }
