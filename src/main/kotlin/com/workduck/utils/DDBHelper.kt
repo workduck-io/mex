adb97d7e1ded52d6c7e3070a3a9e68813a001036
varungarg <varun.garg03@gmail.com>
2022-07-20 23:00:32 +0530

    Remove testing info

--- src/main/kotlin/com/workduck/utils/DDBHelper.kt
+++ src/main/kotlin/com/workduck/utils/DDBHelper.kt
@@ -1,6 +1,7 @@
 package com.workduck.utils
 
 import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.regions.Regions
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
@@ -13,15 +14,15 @@ import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec
 
 object DDBHelper {
 
-    fun createDDBConnection(): AmazonDynamoDB = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
-        AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-east-1"))
-        .build()
-
-//    fun createDDBConnection(): AmazonDynamoDB = AmazonDynamoDBClientBuilder
-//        .standard()
-//        // TODO: read from config file
-//        .withRegion(Regions.US_EAST_1)
+//    fun createDDBConnection(): AmazonDynamoDB = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
+//        AwsClientBuilder.EndpointConfiguration("http://host.docker.internal:8000", "us-east-1"))
 //        .build()
+
+    fun createDDBConnection(): AmazonDynamoDB = AmazonDynamoDBClientBuilder
+        .standard()
+        // TODO: read from config file
+        .withRegion(Regions.US_EAST_1)
+        .build()
 
 
 	/*
