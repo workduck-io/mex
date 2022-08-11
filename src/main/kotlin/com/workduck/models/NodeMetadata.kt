4a3d67858fdf8656a0099005f38f171e0ca0d696
varungarg <varun.garg03@gmail.com>
2022-08-11 08:17:14 +0530

    Add templateID in node metadata

--- src/main/kotlin/com/workduck/models/NodeMetadata.kt
+++ src/main/kotlin/com/workduck/models/NodeMetadata.kt
@@ -1,5 +1,15 @@
 package com.workduck.models
 
+import com.serverless.utils.Constants
+import com.serverless.utils.isValidID
+
 data class NodeMetadata(
-    var iconUrl: String? = null
-)
+    var iconUrl: String? = null,
+
+    var templateID: String? = null // snippet id
+
+) {
+    init {
+        require(templateID?.isValidID(Constants.SNIPPET_ID_PREFIX) ?: true) { "Invalid TemplateID" }
+    }
+}
