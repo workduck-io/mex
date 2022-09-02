370672641e4d55050312896941e0e6fddad718db
varungarg <varun.garg03@gmail.com>
2022-09-02 15:15:30 +0530

    Changes in cold start logic

--- src/main/kotlin/com/serverless/namespaceHandlers/NamespaceHandler.kt
+++ src/main/kotlin/com/serverless/namespaceHandlers/NamespaceHandler.kt
@@ -25,7 +25,7 @@ class NamespaceHandler : RequestHandler<Map<String, Any>, ApiGatewayResponse> {
 
         val json = """
             {
-                "ids" : []
+                "ids" : ["xyz"]
             }
         """.trimIndent()
         val payload: WDRequest? = Helper.objectMapper.readValue(json)
