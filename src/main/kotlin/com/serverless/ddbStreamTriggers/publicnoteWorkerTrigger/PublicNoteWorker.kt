5e9c06ce1d3a18af9658b0b676b3b63945a07745
Rishivikram N <rishi.vikram.1@gmail.com>
2022-09-02 18:36:08 +0530

    Added SQS for handling dead messages

--- src/main/kotlin/com/serverless/ddbStreamTriggers/publicnoteWorkerTrigger/PublicNoteWorker.kt
+++ src/main/kotlin/com/serverless/ddbStreamTriggers/publicnoteWorkerTrigger/PublicNoteWorker.kt
@@ -9,59 +9,45 @@ import com.workduck.models.Node
 import com.workduck.repositories.Cache
 import com.workduck.utils.Helper
 import org.apache.logging.log4j.LogManager
+import com.fasterxml.jackson.module.kotlin.readValue
 
 
 // PublicNoteWorker Lambda is triggered via DDB Streams attached to the node entity.
 class PublicNoteWorker : RequestHandler<DynamodbEvent, Void> {
     private val defaultPublicNoteCacheEndpoint: String = "mex-public-note-cache.m6edlo.ng.0001.use1.cache.amazonaws.com"
     private val cacheExpTimeInSeconds: Long = 900
-    val publicNodeCache: Cache = Cache(System.getenv("PUBLIC_NOTE_CACHE_ENDPOINT") ?: defaultPublicNoteCacheEndpoint)
-
+    private val publicNodeCache: Cache = Cache(System.getenv("PUBLIC_NOTE_CACHE_ENDPOINT") ?: defaultPublicNoteCacheEndpoint)
+    private val dlqURL = System.getenv("SQS_QUEUE_URL")
+    private val sqs = AmazonSQSClientBuilder.defaultClient()
     override fun handleRequest(dynamodbEvent: DynamodbEvent?, context: Context): Void? {
-         val dlqURL = System.getenv("SQS_QUEUE_URL")
-         val sqs = AmazonSQSClientBuilder.defaultClient()
+        if(dynamodbEvent == null || dynamodbEvent.records == null) return null /* will be the case when warmup lambda calls it */
 
-            dynamodbEvent?.records?.let{
-                for (record in dynamodbEvent.records) {
-                    val newImage = record.dynamodb.newImage
-                    LOG.debug(newImage.toString())
-                    val nodeID = newImage["SK"]?.s ?: throw Exception("Invalid Record. NodeID not available")
-                    val publicAccess = newImage["publicAccess"]?.n ?: throw Exception("Invalid Record. PublicAccess not available")
-                    LOG.debug(publicAccess.toString())
-                    // Check for public access update for the node
-                    if(publicAccess == "1") {
-                        val jsonResult = Helper.mapToJson(newImage).toMutableMap()
-                        LOG.debug("jsonresuikt ${Helper.objectMapper.writeValueAsString(jsonResult)}")
-                        try {
-                            val nodeObject : Node = Helper.objectMapper.convertValue(jsonResult, Node::class.java)
-                            val existingPublicNote = publicNodeCache.get(nodeID.toString())
-
-                            LOG.debug("existingnote ${existingPublicNote.toString()}")
-
-                            if(existingPublicNote != null) {
-                                val existingNodeObject : Node = Helper.objectMapper.convertValue(existingPublicNote, Node::class.java)
-                                if(existingNodeObject.updatedAt < nodeObject.updatedAt)
-                                    publicNodeCache.set(nodeID.toString(), cacheExpTimeInSeconds, nodeObject.toString())
-                            } else {
-                                LOG.debug("before set")
-                                publicNodeCache.set(nodeID.toString(), cacheExpTimeInSeconds, nodeObject.toString())
-                                LOG.debug("after set")
-                            }
-                        } catch (ex: Exception) {
-                            LOG.error(ex)
-                            LOG.info("Sending to dead letter queue")
-                            LOG.debug("josnResult ${jsonResult.toString()}")
-                            LOG.debug("queueurl ${dlqURL}")
-                            val sendMsgRequest = SendMessageRequest()
-                                .withQueueUrl(dlqURL)
-                                .withMessageBody(jsonResult.toString())
-                            LOG.debug("sendMsgRequest.toString()")
-                            sqs.sendMessage(sendMsgRequest)
-                        }
+        for (record in dynamodbEvent.records) {
+            val newImage = record.dynamodb.newImage
+            val nodeID = newImage["SK"]?.s ?: throw Exception("Invalid Record. NodeID not available")
+            val jsonResult = Helper.mapToJson(newImage).toMutableMap()
+            val nodeObject : Node = Helper.objectMapper.convertValue(jsonResult, Node::class.java)
+            try {
+                // Check for public access update for the node
+                if(nodeObject.publicAccess) {
+                    val existingPublicNote = publicNodeCache.get(nodeID)
+                    if(existingPublicNote != null) {
+                        val existingNode : Node = Helper.objectMapper.readValue(existingPublicNote)
+                        if(existingNode.updatedAt < nodeObject.updatedAt)
+                            publicNodeCache.set(nodeID, cacheExpTimeInSeconds, Helper.objectMapper.writeValueAsString(jsonResult))
+                    } else {
+                        publicNodeCache.set(nodeID, cacheExpTimeInSeconds, Helper.objectMapper.writeValueAsString(jsonResult))
                     }
                 }
+                publicNodeCache.closeConnection()
+            } catch (ex: Exception) {
+                LOG.error(ex.message.toString())
+                val sendMsgRequest = SendMessageRequest()
+                                    .withQueueUrl(dlqURL)
+                                    .withMessageBody(jsonResult.toString())
+                sqs.sendMessage(sendMsgRequest)
             }
-        publicNodeCache.closeConnection()
+        }
         return null
     }
 
--- src/main/kotlin/com/serverless/sqsTriggers/publicnoteSQSWorkerTrigger/PublicNoteSQSWorker.kt
+++ src/main/kotlin/com/serverless/sqsTriggers/publicnoteSQSWorkerTrigger/PublicNoteSQSWorker.kt
@@ -3,8 +3,7 @@ package com.serverless.sqsTriggers.publicnoteSQSWorkerTrigger
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder
-import com.amazonaws.services.sqs.model.Message
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.workduck.models.Node
 import com.workduck.repositories.Cache
 import com.workduck.utils.Helper
@@ -13,30 +12,32 @@ import org.apache.logging.log4j.LogManager
 class PublicNoteSQSWorker: RequestHandler<SQSEvent, Void> {
     private val defaultPublicNoteCacheEndpoint: String = "mex-public-note-cache.m6edlo.ng.0001.use1.cache.amazonaws.com"
     private val cacheExpTimeInSeconds: Long = 900
-
+    private val publicNodeCache: Cache = Cache(System.getenv("PUBLIC_NOTE_CACHE_ENDPOINT") ?: defaultPublicNoteCacheEndpoint)
     override fun handleRequest(input: SQSEvent?, context: Context?): Void? {
-        val publicNodeCache: Cache = Cache(System.getenv("PUBLIC_NOTE_CACHE_ENDPOINT") ?: defaultPublicNoteCacheEndpoint)
-
-        LOG.debug("insidee ${input.toString()}")
 
         input?.records?.let{
             for (record in input.records) {
-                val nodeObject : Node = Helper.objectMapper.convertValue(record, Node::class.java)
+                val jsonResult = record.body as MutableMap<String?, Any?>
+                val nodeObject : Node = Helper.objectMapper.convertValue(jsonResult, Node::class.java)
                 val nodeID = nodeObject.id
-                val existingPublicNote = publicNodeCache.get(nodeID.toString())
-                LOG.debug(nodeObject.toString())
+
                 try {
-                    if(existingPublicNote != null) {
-                        val existingNodeObject : Node = Helper.objectMapper.convertValue(existingPublicNote, Node::class.java)
-                        if(existingNodeObject.updatedAt < nodeObject.updatedAt)
-                            publicNodeCache.set(nodeID.toString(), cacheExpTimeInSeconds, nodeObject.toString())
-                    } else publicNodeCache.set(nodeID.toString(), cacheExpTimeInSeconds, nodeObject.toString())
+                    if(nodeObject.publicAccess) {
+                        val existingPublicNote = publicNodeCache.get(nodeID)
+                        if(existingPublicNote != null) {
+                            val existingNode : Node = Helper.objectMapper.readValue(existingPublicNote)
+                            if(existingNode.updatedAt < nodeObject.updatedAt)
+                                publicNodeCache.set(nodeID, cacheExpTimeInSeconds, Helper.objectMapper.writeValueAsString(jsonResult))
+                        } else {
+                            publicNodeCache.set(nodeID, cacheExpTimeInSeconds, Helper.objectMapper.writeValueAsString(jsonResult))
+                        }
+                    }
+                    publicNodeCache.closeConnection()
                 } catch (ex: Exception) {
                     LOG.error(ex.message.toString())
                 }
             }
         }
-        publicNodeCache.closeConnection()
         return null
     }
 
