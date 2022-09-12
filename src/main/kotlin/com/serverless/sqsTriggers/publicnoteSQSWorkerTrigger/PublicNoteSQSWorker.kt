d65e524540a9dfb46bdb456cc1a04ac2ad772de0
Rishivikram N <rishi.vikram.1@gmail.com>
2022-09-12 22:52:20 +0530

    Review changes done

--- src/main/kotlin/com/serverless/sqsTriggers/publicnoteSQSWorkerTrigger/PublicNoteSQSWorker.kt
+++ src/main/kotlin/com/serverless/sqsTriggers/publicnoteSQSWorkerTrigger/PublicNoteSQSWorker.kt
@@ -5,13 +5,13 @@ import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
 import com.serverless.utils.Constants
 import com.workduck.models.Node
-import com.workduck.repositories.Cache
+import com.workduck.repositories.NodeCache
 import com.workduck.utils.Helper
 import org.apache.logging.log4j.LogManager
 
 class PublicNoteSQSWorker: RequestHandler<SQSEvent, Void> {
     companion object {
-        private val publicNodeCache: Cache<Node> = Cache(System.getenv("PUBLIC_NOTE_CACHE_ENDPOINT") ?: Constants.DEFAULT_PUBLIC_NOTE_CACHE_ENDPOINT)
+        private val publicNodeCache = NodeCache(System.getenv("PUBLIC_NOTE_CACHE_ENDPOINT") ?: Constants.DEFAULT_PUBLIC_NOTE_CACHE_ENDPOINT)
         private val LOG = LogManager.getLogger(PublicNoteSQSWorker::class.java)
     }
     override fun handleRequest(sqsEvent: SQSEvent?, context: Context?): Void? {
@@ -22,25 +22,21 @@ class PublicNoteSQSWorker: RequestHandler<SQSEvent, Void> {
                     val node: Node = nodeString.toNode()
 
                     try {
-                        takeIf { node.hasPublicAccess() }.apply {
-                            //checked for value existing in cache
-                            publicNodeCache.getItem(node.id)
-                                ?.also { existingNode ->
-                                    if (existingNode.isOlderVariant(node)) {
-                                        publicNodeCache.setItem(
-                                            node.id,
-                                            Constants.PUBLIC_NOTE_EXP_TIME_IN_SECONDS,
-                                            node
-                                        )
-                                    }
-                                } ?: publicNodeCache.setItem(
-                                node.id,
-                                Constants.PUBLIC_NOTE_EXP_TIME_IN_SECONDS,
-                                node
-                            )
-
-                        }
-
+                        //checked for value existing in cache
+                        publicNodeCache.getItem(node.id)
+                            ?.also { existingNode ->
+                                if (existingNode.isOlderVariant(node)) {
+                                    publicNodeCache.setItem(
+                                        node.id,
+                                        Constants.PUBLIC_NOTE_EXP_TIME_IN_SECONDS,
+                                        node
+                                    )
+                                }
+                            } ?: publicNodeCache.setItem(
+                            node.id,
+                            Constants.PUBLIC_NOTE_EXP_TIME_IN_SECONDS,
+                            node
+                        )
                     } catch (ex: Exception) {
                         LOG.error(ex.message.toString())
                     } finally {
