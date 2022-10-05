b7607f0649db17ddc9a447975d78445a621489e2
varungarg <varun.garg03@gmail.com>
2022-10-05 16:09:14 +0530

    Remove Comment Support from mex-backend

--- src/main/kotlin/com/serverless/commentHandlers/CommentStrategy.kt
+++
@@ -1,9 +0,0 @@
-package com.serverless.commentHandlers
-
-import com.serverless.ApiGatewayResponse
-import com.serverless.models.Input
-import com.workduck.service.CommentService
-
-interface CommentStrategy {
-    fun apply(input: Input, commentService: CommentService): ApiGatewayResponse
-}
