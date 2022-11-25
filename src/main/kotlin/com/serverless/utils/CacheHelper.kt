573967286ae24ea6a705193f509d4d1956bbfe42
Rishivikram N <rishi.vikram.1@gmail.com>
2022-11-25 20:44:43 +0530

    rollbacked the imports

--- src/main/kotlin/com/serverless/utils/CacheHelper.kt
+++ src/main/kotlin/com/serverless/utils/CacheHelper.kt
@@ -4,6 +4,6 @@ object CacheHelper {
     const val CACHE_DELIMITER = '+'
 
     fun encodePublicCacheKey(nodeId: String): String {
-        return "PUBLIC${CACHE_DELIMITER}${nodeId}"
+        return "MEX-BACKEND${CACHE_DELIMITER}${nodeId}"
     }
 }
