f8fee2ed7d92e728023b1db710d46de7978f6f9b
Varun Garg <varun.garg03@gmail.com>
2023-02-10 15:38:41 +0530

    Remove register user flow (#209)
    https://github.com/workduck-io/mex/pull/209

--- src/main/kotlin/com/serverless/userHandlers/UserStrategy.kt
+++
@@ -1,9 +0,0 @@
-package com.serverless.userHandlers
-
-import com.serverless.ApiGatewayResponse
-import com.serverless.models.Input
-import com.workduck.service.UserService
-
-interface UserStrategy {
-    fun apply(input: Input, userService: UserService): ApiGatewayResponse
-}
