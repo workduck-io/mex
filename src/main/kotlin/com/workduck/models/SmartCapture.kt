a14272e6b5f8eb44a5764086fe92a54c80faf42e
Shubham Mittal <shubham9910103545@gmail.com>
2023-02-24 20:06:23 +0530

    Setting uo a skeleton interface 2

---
+++ src/main/kotlin/com/workduck/models/SmartCapture.kt
@@ -0,0 +1,104 @@
+package com.workduck.models
+
+import com.amazonaws.services.dynamodbv2.datamodeling.*
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.serverless.utils.Constants
+import com.workduck.converters.*
+import com.workduck.utils.Helper
+
+data class SmartCapture(
+
+    @JsonProperty("workspaceIdentifier")
+    @JsonDeserialize(converter = WorkspaceIdentifierDeserializer::class)
+    @JsonSerialize(converter = IdentifierSerializer::class)
+    @DynamoDBTypeConverted(converter = WorkspaceIdentifierConverter::class)
+    @DynamoDBHashKey(attributeName = "PK")
+    override var workspaceIdentifier: WorkspaceIdentifier = WorkspaceIdentifier("DefaultWorkspace"),
+
+    @JsonProperty("version")
+    @DynamoDBAttribute(attributeName = "version")
+    override var version: Int? = 1,
+
+    @JsonProperty("id")
+    var id: String = Helper.generateNanoID(IdentifierType.SMART_CAPTURE.name),
+
+    @JsonProperty("itemType")
+    @DynamoDBAttribute(attributeName = "itemType")
+    @DynamoDBTypeConverted(converter = ItemTypeConverter::class)
+    override var itemType: ItemType = ItemType.Snippet,
+
+    @JsonProperty("title")
+    @DynamoDBAttribute(attributeName = "title")
+    override var title: String = "New Snippet",
+
+    @JsonProperty("itemStatus")
+    @DynamoDBAttribute(attributeName = "itemStatus")
+    @DynamoDBTypeConverted(converter = ItemStatusConverter::class)
+    var itemStatus: ItemStatus = ItemStatus.ACTIVE,
+
+    @JsonProperty("data")
+    @DynamoDBTypeConverted(converter = NodeDataConverter::class)
+    @DynamoDBAttribute(attributeName = "nodeData")
+    override var data: List<BlockElement>? = null,
+
+    @JsonProperty("metadata")
+    @JsonSerialize(converter = PageMetadataSerializer::class)
+    @JsonDeserialize(converter = PageMetadataDeserializer::class)
+    @DynamoDBTypeConverted(converter = PageMetadataConverter::class)
+    @DynamoDBAttribute(attributeName = "metadata")
+    override var metadata: PageMetadata?= null,
+
+    @DynamoDBAttribute(attributeName = "dataOrder")
+    override var dataOrder: MutableList<String>? = null,
+
+    @DynamoDBAttribute(attributeName = "template")
+    var template: Boolean? = null,
+
+    @JsonProperty("createdBy")
+    @DynamoDBAttribute(attributeName = "createdBy")
+    override var createdBy: String? = null,
+
+    @JsonProperty("lastEditedBy")
+    @DynamoDBAttribute(attributeName = "lastEditedBy")
+    override var lastEditedBy: String? = null,
+
+    @JsonProperty("referenceSnippet")
+    @DynamoDBAttribute(attributeName = "referenceSnippet")
+    @DynamoDBTypeConverted(converter = SnippetIdentifierConverter::class)
+    var referenceSnippet : SnippetIdentifier? = null,
+
+//    @JsonProperty("namespaceIdentifier")
+//    @JsonDeserialize(converter = NamespaceIdentifierDeserializer::class)
+//    @JsonSerialize(converter = IdentifierSerializer::class)
+//    @DynamoDBTypeConverted(converter = NamespaceIdentifierConverter::class)
+//    @DynamoDBAttribute(attributeName = "namespaceIdentifier")
+//    override var namespaceIdentifier: NamespaceIdentifier? = null,
+
+    @JsonProperty("publicAccess")
+    @DynamoDBAttribute(attributeName = "publicAccess")
+    override var publicAccess: Boolean = false,
+
+    @JsonProperty("createdAt")
+    @DynamoDBAttribute(attributeName = "createdAt")
+    override var createdAt: Long? = Constants.getCurrentTime()
+
+) : Entity, Page<BlockElement> {
+
+    @JsonProperty("updatedAt")
+    @DynamoDBAttribute(attributeName = "updatedAt")
+    override var updatedAt: Long = Constants.getCurrentTime()
+
+    @JsonProperty("sk")
+    @DynamoDBRangeKey(attributeName = "SK")
+    var sk: String = "$id${Constants.DELIMITER}$version"
+
+    companion object {
+        fun setCreatedFieldsNull(page: Page<BlockElement>){
+            page.createdAt = null
+            page.createdBy = null
+        }
+    }
+}
+
