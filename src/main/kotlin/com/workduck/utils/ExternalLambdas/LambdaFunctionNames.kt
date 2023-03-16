52d208b13c7985f08849c01ad06a9a6cd4a17125
Rishivikram N <rishi.vikram.1@gmail.com>
2023-03-16 16:13:17 +0530

    Lambda function names

---
+++ src/main/kotlin/com/workduck/utils/ExternalLambdas/LambdaFunctionNames.kt
@@ -0,0 +1,6 @@
+package com.workduck.utils.ExternalLambdas
+
+object LambdaFunctionNames {
+//    var CREATE_CAPTURE_LAMBDA = "smartcapture-${System.getenv("STAGE")}-capture"
+    var CREATE_CAPTURE_LAMBDA = "smartcapture-test-capture"
+}
