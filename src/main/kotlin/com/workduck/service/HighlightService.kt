346d84919677250a89f9d7436c826a55f9ea093a
Rishivikram N <rishi.vikram.1@gmail.com>
2023-05-10 18:08:22 +0530

    Refactored to create an entity service

--- src/main/kotlin/com/workduck/service/HighlightService.kt
+++ src/main/kotlin/com/workduck/service/HighlightService.kt
@@ -1,164 +1,113 @@
 package com.workduck.service
 
-import com.fasterxml.jackson.core.type.TypeReference
 import com.serverless.models.requests.*
 import com.serverless.utils.Constants
-import com.serverless.utils.Messages
 import com.workduck.models.AdvancedElement
-import com.workduck.models.EntityOperationType
 import com.workduck.models.entityServiceResponses.MultipleEntityPaginatedResponse
-import com.workduck.service.serviceUtils.ServiceUtils.populateEntityMetadata
-import com.workduck.service.serviceUtils.ServiceUtils.invokeCreateOrUpdateEntityLambda
-import com.workduck.service.serviceUtils.ServiceUtils.getNodeIDWorkspaceID
-import com.workduck.service.serviceUtils.ServiceUtils.invokeCreateInstanceEntityLambda
-import com.workduck.service.serviceUtils.ServiceUtils.invokeGetEntityLambda
-import com.workduck.service.serviceUtils.ServiceUtils.invokeDeleteEntityLambda
-import com.workduck.service.serviceUtils.ServiceUtils.invokeGetAllEntityLambda
-import com.workduck.service.serviceUtils.ServiceUtils.invokeGetAllEntitiesByIDSLambda
-import com.workduck.service.serviceUtils.ServiceUtils.invokeGetAllEntityInstancesByIDLambda
-import com.workduck.utils.EntityHelper
-import com.workduck.utils.Helper
 import com.workduck.utils.externalLambdas.HttpMethods
 import com.workduck.utils.externalLambdas.LambdaFunctionNames
 import com.workduck.utils.externalLambdas.RoutePaths
 
-class HighlightService(
-    private val nodeService : NodeService = NodeService()
-){
-    fun createHighlight(wdRequest: WDRequest?, userID: String, userWorkspaceID: String, parentHighlightID: String?) : String {
-        val request = wdRequest as EntityTypeRequest
-        // this contains the nodeID to which highlight should be appended and the workspaceID of that node.
-        val nodeWorkspaceMap = getNodeIDWorkspaceID(nodeService, request.nodeNamespaceMap, userID, userWorkspaceID)
-        val defaultBlockID: String
-        val newHighlightID: String
-
+class HighlightService(): EntityService() {
+    fun createHighlight(wdRequest: WDRequest, userID: String, userWorkspaceID: String, parentHighlightID: String?) : String {
         // This block handles the usual highlight creation
         if(parentHighlightID == null) {
-            val highlight: AdvancedElement = request.data!!
-            populateEntityMetadata(highlight, userID, createdAt = Constants.getCurrentTime(), createdBy = userID)
-            newHighlightID = invokeCreateOrUpdateEntityLambda(
-                highlight,
-                nodeWorkspaceMap.workspaceID,
-                userID,
-                LambdaFunctionNames.HIGHLIGHT_LAMBDA,
-                RoutePaths.CREATE_OR_UPDATE_OR_INSTANTIATE_HIGHLIGHT,
-                HttpMethods.POST).id
-            defaultBlockID = highlight.id
+            val highlightID = createOrUpdateEntity(
+                wdRequest = wdRequest,
+                userID = userID,
+                userWorkspaceID = userWorkspaceID,
+                lambdaFunctionName = LambdaFunctionNames.HIGHLIGHT_LAMBDA,
+                routePath = RoutePaths.CREATE_OR_UPDATE_OR_INSTANTIATE_HIGHLIGHT,
+                httpMethod = HttpMethods.POST,
+                elementType = Constants.ELEMENT_HIGHLIGHT,
+                entityID = null
+            )
+            return highlightID!!
         }
         // This block handles the highlight instance creation from the given the parent highlight id
-        else {
-            newHighlightID = invokeCreateInstanceEntityLambda(
-                parentHighlightID,
-                nodeWorkspaceMap.workspaceID,
-                userID,
-                LambdaFunctionNames.HIGHLIGHT_LAMBDA,
-                RoutePaths.CREATE_OR_UPDATE_OR_INSTANTIATE_HIGHLIGHT,
-                HttpMethods.POST,
-                request.data).id
-            defaultBlockID = Constants.DEFAULT_VALUE
-        }
-        val refBlock = EntityHelper.createEntityReferenceBlock(defaultBlockID, newHighlightID, Constants.ELEMENT_HIGHLIGHT)
-        nodeService.appendEntityBlocks(nodeWorkspaceMap.nodeID, nodeWorkspaceMap.workspaceID, userID, listOf(refBlock))
-
-        return newHighlightID
+        else
+            return createEntityInstance(
+                wdRequest = wdRequest,
+                userID = userID,
+                userWorkspaceID = userWorkspaceID,
+                lambdaFunctionName = LambdaFunctionNames.HIGHLIGHT_LAMBDA,
+                routePath = RoutePaths.CREATE_OR_UPDATE_OR_INSTANTIATE_HIGHLIGHT,
+                httpMethod = HttpMethods.POST,
+                elementType = Constants.ELEMENT_HIGHLIGHT,
+                parentID = parentHighlightID
+            )
     }
 
     fun updateHighlight(highlightID: String, wdRequest: WDRequest, userID: String, userWorkspaceID: String) {
-        val request = wdRequest as EntityTypeRequest
-        val nodeWorkspaceMap = getNodeIDWorkspaceID(nodeService, request.nodeNamespaceMap, userID, userWorkspaceID)
-        val highlight: AdvancedElement = request.data!!
-        populateEntityMetadata(highlight, userID, createdAt = null, createdBy = null)
-        invokeCreateOrUpdateEntityLambda(
-            highlight,
-            nodeWorkspaceMap.workspaceID,
-            userID,
-            LambdaFunctionNames.HIGHLIGHT_LAMBDA,
-            RoutePaths.CREATE_OR_UPDATE_OR_INSTANTIATE_HIGHLIGHT,
-            HttpMethods.POST,
-            entityID = highlightID).id
+        createOrUpdateEntity(
+            wdRequest =  wdRequest,
+            userID = userID,
+            userWorkspaceID = userWorkspaceID,
+            lambdaFunctionName = LambdaFunctionNames.HIGHLIGHT_LAMBDA,
+            routePath = RoutePaths.CREATE_OR_UPDATE_OR_INSTANTIATE_HIGHLIGHT,
+            httpMethod = HttpMethods.POST,
+            elementType = Constants.ELEMENT_HIGHLIGHT,
+            entityID = highlightID
+        )
     }
 
     fun getHighlight(highlightID: String, nodeID: String, namespaceID: String, userWorkspaceID: String, userID: String): AdvancedElement? {
-        val workspaceID = nodeService.nodeAccessService
-            .checkIfUserHasAccessAndGetWorkspaceDetails(
-                nodeID,
-                userWorkspaceID,
-                namespaceID,
-                userID,
-                EntityOperationType.WRITE
-            ).let { workspaceDetails ->
-                require(!workspaceDetails[Constants.WORKSPACE_ID].isNullOrEmpty()) { Messages.ERROR_NODE_PERMISSION }
-                workspaceDetails[Constants.WORKSPACE_ID]!!
-            }
-        return Helper.objectMapper.readValue(
-                invokeGetEntityLambda(
-                    workspaceID,
-                    userID,
-                    highlightID,
-                    LambdaFunctionNames.HIGHLIGHT_LAMBDA,
-                    RoutePaths.GET_HIGHLIGHT,
-                    HttpMethods.GET
-                ),
-                AdvancedElement::class.java
-                )
+        return getEntity(
+            userID = userID,
+            userWorkspaceID = userWorkspaceID,
+            nodeID = nodeID,
+            namespaceID = namespaceID,
+            lambdaFunctionName = LambdaFunctionNames.HIGHLIGHT_LAMBDA,
+            routePath = RoutePaths.GET_HIGHLIGHT,
+            httpMethod = HttpMethods.GET,
+            entityID = highlightID
+        )
     }
 
 
     fun deleteHighlight(highlightID: String, nodeID: String, namespaceID: String, userWorkspaceID: String, userID: String) {
-        val workspaceID = nodeService.nodeAccessService
-            .checkIfUserHasAccessAndGetWorkspaceDetails(
-                nodeID,
-                userWorkspaceID,
-                namespaceID,
-                userID,
-                EntityOperationType.WRITE
-            ).let { workspaceDetails ->
-                require(!workspaceDetails[Constants.WORKSPACE_ID].isNullOrEmpty()) { Messages.ERROR_NODE_PERMISSION }
-                workspaceDetails[Constants.WORKSPACE_ID]!!
-            }
-
-        invokeDeleteEntityLambda(
-            workspaceID,
-            userID,
-            highlightID,
-            LambdaFunctionNames.HIGHLIGHT_LAMBDA,
-            RoutePaths.DELETE_HIGHLIGHT,
-            HttpMethods.DELETE
+        deleteEntity(
+            userID = userID,
+            userWorkspaceID = userWorkspaceID,
+            nodeID = nodeID,
+            namespaceID = namespaceID,
+            lambdaFunctionName = LambdaFunctionNames.HIGHLIGHT_LAMBDA,
+            routePath = RoutePaths.DELETE_HIGHLIGHT,
+            httpMethod = HttpMethods.DELETE,
+            entityID = highlightID
         )
     }
 
     fun getAllHighlights(workspaceID: String, userID: String, lastKey: String? = null): MultipleEntityPaginatedResponse<AdvancedElement> {
-        return Helper.objectMapper.readValue(
-            invokeGetAllEntityLambda(
-                    workspaceID,
-                    userID,
-                    LambdaFunctionNames.HIGHLIGHT_LAMBDA,
-                    RoutePaths.GET_ALL_HIGHLIGHTS,
-                    HttpMethods.GET,
-                    lastKey), object : TypeReference<MultipleEntityPaginatedResponse<AdvancedElement>>() {})
+        return getAllEntitiesForWorkspace(
+            userID = userID,
+            userWorkspaceID = workspaceID,
+            lambdaFunctionName = LambdaFunctionNames.HIGHLIGHT_LAMBDA,
+            routePath = RoutePaths.GET_ALL_HIGHLIGHTS,
+            httpMethod = HttpMethods.GET,
+            lastKey
+        )
     }
 
     fun getAllHighlightInstances(workspaceID: String, userID: String, highlightID: String): MultipleEntityPaginatedResponse<AdvancedElement> {
-        return Helper.objectMapper.readValue(
-            invokeGetAllEntityInstancesByIDLambda(
-                highlightID,
-                workspaceID,
-                userID,
-                LambdaFunctionNames.HIGHLIGHT_LAMBDA,
-                RoutePaths.GET_ALL_HIGHLIGHT_INSTANCES,
-                HttpMethods.GET), object : TypeReference<MultipleEntityPaginatedResponse<AdvancedElement>>() {})
+        return getAllEntityInstances(
+            userID,
+            userWorkspaceID = workspaceID,
+            lambdaFunctionName = LambdaFunctionNames.HIGHLIGHT_LAMBDA,
+            routePath = RoutePaths.GET_ALL_HIGHLIGHT_INSTANCES,
+            httpMethod = HttpMethods.GET,
+            parentID = highlightID
+        )
     }
 
     fun getAllHighlightsByIDs(wdRequest: WDRequest, userID: String, userWorkspaceID: String): MultipleEntityPaginatedResponse<AdvancedElement> {
-        val request = wdRequest as GenericListRequest
-
-        return Helper.objectMapper.readValue(
-            invokeGetAllEntitiesByIDSLambda(
-                    request,
-                    userWorkspaceID,
-                    userID,
-                    LambdaFunctionNames.HIGHLIGHT_LAMBDA,
-                    RoutePaths.GET_MULTIPLE_HIGHLIGHTS,
-                    HttpMethods.POST), object : TypeReference<MultipleEntityPaginatedResponse<AdvancedElement>>() {})
+        return getAllEntitiesByIDs(
+            wdRequest = wdRequest,
+            userID = userID,
+            userWorkspaceID = userWorkspaceID,
+            lambdaFunctionName = LambdaFunctionNames.HIGHLIGHT_LAMBDA,
+            routePath = RoutePaths.GET_MULTIPLE_HIGHLIGHTS,
+            httpMethod = HttpMethods.POST
+        )
     }
 }
--- src/main/kotlin/com/workduck/service/SmartCaptureService.kt
+++ src/main/kotlin/com/workduck/service/SmartCaptureService.kt
@@ -1,121 +1,67 @@
 package com.workduck.service
 
-import com.fasterxml.jackson.core.type.TypeReference
 import com.serverless.models.requests.MoveEntityRequest
-import com.serverless.models.requests.EntityTypeRequest
 import com.serverless.models.requests.WDRequest
 import com.serverless.utils.Constants
 import com.serverless.utils.Messages
 import com.serverless.utils.extensions.isValidCaptureID
 import com.workduck.models.AdvancedElement
-import com.workduck.models.EntityOperationType
-import com.workduck.models.EntityServiceCreateResponse
-import com.workduck.models.Node
 import com.workduck.models.entityServiceResponses.MultipleEntityPaginatedResponse
-import com.workduck.models.entityServiceResponses.SingleEntityResponse
-import com.workduck.service.serviceUtils.ServiceUtils.getNodeIDWorkspaceID
-import com.workduck.service.serviceUtils.ServiceUtils.populateEntityMetadata
-import com.workduck.service.serviceUtils.ServiceUtils.invokeCreateOrUpdateEntityLambda
-import com.workduck.service.serviceUtils.ServiceUtils.invokeUpdateEntityLambda
-import com.workduck.service.serviceUtils.ServiceUtils.invokeGetEntityLambda
-import com.workduck.service.serviceUtils.ServiceUtils.invokeDeleteEntityLambda
-import com.workduck.service.serviceUtils.ServiceUtils.invokeGetEntitiesWithFilterLambda
-import com.workduck.utils.EntityHelper
-import com.workduck.utils.Helper
 import com.workduck.utils.externalLambdas.HttpMethods
 import com.workduck.utils.externalLambdas.LambdaFunctionNames
 import com.workduck.utils.externalLambdas.RoutePaths
 
-class SmartCaptureService (
-    private val nodeService : NodeService = NodeService()
-){
+class SmartCaptureService (): EntityService(){
     fun createSmartCapture(wdRequest: WDRequest, userID: String, userWorkspaceID: String) : String {
-        val request = wdRequest as EntityTypeRequest
-
-        // this contains the nodeID to which smartCapture should be appended and the workspaceID of that node.
-        val nodeWorkspaceMap = getNodeIDWorkspaceID(nodeService, request.nodeNamespaceMap, userID, userWorkspaceID)
-
-        val smartCapture: AdvancedElement = request.data!!
-        populateEntityMetadata(smartCapture, userID, createdAt = Constants.getCurrentTime(), createdBy = userID)
-
-        val captureID = invokeCreateOrUpdateEntityLambda(
-                            smartCapture,
-                            nodeWorkspaceMap.workspaceID,
-                            userID,
-                            LambdaFunctionNames.CAPTURE_LAMBDA,
-                            RoutePaths.CREATE_CAPTURE,
-                            HttpMethods.POST).id
-        val refBlock = EntityHelper.createEntityReferenceBlock(smartCapture.id, captureID, Constants.ELEMENT_SMART_CAPTURE)
-        nodeService.appendEntityBlocks(nodeWorkspaceMap.nodeID, nodeWorkspaceMap.workspaceID, userID, listOf(refBlock))
-        return captureID
+        val captureID = createOrUpdateEntity(
+            wdRequest = wdRequest,
+            userID = userID,
+            userWorkspaceID = userWorkspaceID,
+            lambdaFunctionName = LambdaFunctionNames.CAPTURE_LAMBDA,
+            routePath = RoutePaths.CREATE_CAPTURE,
+            httpMethod = HttpMethods.POST,
+            elementType = Constants.ELEMENT_SMART_CAPTURE,
+            entityID = null
+        )
+        return captureID!!
     }
 
-
     fun updateSmartCapture(smartCaptureID: String, wdRequest: WDRequest, userID: String, userWorkspaceID: String) {
-        val request = wdRequest as EntityTypeRequest
-
-        // this contains the nodeID to which smartCapture should be appended and the workspaceID of that node.
-        val nodeWorkspaceMap = getNodeIDWorkspaceID(nodeService, request.nodeNamespaceMap, userID, userWorkspaceID)
-
-        val smartCapture: AdvancedElement = request.data!!
-
-        populateEntityMetadata(smartCapture, userID, createdAt = null, createdBy = null)
-        invokeUpdateEntityLambda(
-            smartCaptureID,
-            smartCapture,
-            nodeWorkspaceMap.workspaceID,
-            userID,
-            LambdaFunctionNames.CAPTURE_LAMBDA,
-            RoutePaths.UPDATE_CAPTURE,
-            HttpMethods.PATCH)
+        updateEntity(
+            wdRequest =  wdRequest,
+            userID = userID,
+            userWorkspaceID = userWorkspaceID,
+            lambdaFunctionName = LambdaFunctionNames.CAPTURE_LAMBDA,
+            routePath = RoutePaths.UPDATE_CAPTURE,
+            httpMethod = HttpMethods.PATCH,
+            entityID = smartCaptureID
+        )
     }
 
     fun getSmartCapture(captureID: String, nodeID: String, namespaceID: String, userWorkspaceID: String, userID: String): AdvancedElement? {
-        val workspaceID = nodeService.nodeAccessService
-            .checkIfUserHasAccessAndGetWorkspaceDetails(
-                nodeID,
-                userWorkspaceID,
-                namespaceID,
-                userID,
-                EntityOperationType.WRITE
-            ).let { workspaceDetails ->
-                require(!workspaceDetails[Constants.WORKSPACE_ID].isNullOrEmpty()) { Messages.ERROR_NODE_PERMISSION }
-                workspaceDetails[Constants.WORKSPACE_ID]!!
-            }
-
-        return Helper.objectMapper.readValue(
-            invokeGetEntityLambda(
-                workspaceID,
-                userID,
-                captureID,
-                LambdaFunctionNames.CAPTURE_LAMBDA,
-                RoutePaths.GET_CAPTURE,
-                HttpMethods.GET),
-                SingleEntityResponse::class.java
-                ).data
+        return getEntity(
+            userID = userID,
+            userWorkspaceID = userWorkspaceID,
+            nodeID = nodeID,
+            namespaceID = namespaceID,
+            lambdaFunctionName = LambdaFunctionNames.CAPTURE_LAMBDA,
+            routePath = RoutePaths.GET_CAPTURE,
+            httpMethod = HttpMethods.GET,
+            entityID = captureID
+        )
     }
 
 
     fun deleteSmartCapture(captureID: String, nodeID: String, namespaceID: String, userWorkspaceID: String, userID: String) {
-        val workspaceID = nodeService.nodeAccessService
-            .checkIfUserHasAccessAndGetWorkspaceDetails(
-                nodeID,
-                userWorkspaceID,
-                namespaceID,
-                userID,
-                EntityOperationType.WRITE
-            ).let { workspaceDetails ->
-                require(!workspaceDetails[Constants.WORKSPACE_ID].isNullOrEmpty()) { Messages.ERROR_NODE_PERMISSION }
-                workspaceDetails[Constants.WORKSPACE_ID]!!
-            }
-
-        invokeDeleteEntityLambda(
-            workspaceID,
-            userID,
-            captureID,
-            LambdaFunctionNames.CAPTURE_LAMBDA,
-            RoutePaths.DELETE_CAPTURE,
-            HttpMethods.DELETE
+        deleteEntity(
+            userID = userID,
+            userWorkspaceID = userWorkspaceID,
+            nodeID = nodeID,
+            namespaceID = namespaceID,
+            lambdaFunctionName = LambdaFunctionNames.CAPTURE_LAMBDA,
+            routePath = RoutePaths.DELETE_CAPTURE,
+            httpMethod = HttpMethods.DELETE,
+            entityID = captureID
         )
     }
 
@@ -124,45 +70,28 @@ class SmartCaptureService (
         val request = wdRequest as MoveEntityRequest
         val captureID = request.entityID
         require(captureID.isValidCaptureID()) { Messages.INVALID_CAPTURE_ID }
-
-
-        // this map contains the nodeID to which smartCapture should be moved and the workspaceID of that node.
-        val nodeWorkspaceMap = getNodeIDWorkspaceID(nodeService, request.nodeNamespaceMap, userID, userWorkspaceID)
-
-        //TODO(ask directly for blockID from entity service)
-
-        val blockID = Helper.objectMapper.readValue(
-            invokeGetEntityLambda(
-                userWorkspaceID,
-                userID,
-                captureID,
-                LambdaFunctionNames.CAPTURE_LAMBDA,
-                RoutePaths.GET_CAPTURE,
-                HttpMethods.GET),
-                SingleEntityResponse::class.java).data.id
-
-        val sourceNodeWithBlockAndDataOrder: Node = nodeService.nodeRepository.getNodeWithBlockAndDataOrder(Constants.SMART_CAPTURE_DEFAULT_NODE_ID, blockID, userWorkspaceID).let{ node ->
-            require(node != null) { Messages.INVALID_NODE_ID }
-            require(node.data?.get(0) != null ) { Messages.INVALID_BLOCK_ID }
-            check(!node.dataOrder.isNullOrEmpty()) {Messages.INVALID_NODE_STATE}
-            node
-        }
-
-        sourceNodeWithBlockAndDataOrder.dataOrder!!.let { dataOrder ->
-            nodeService.nodeRepository.moveBlock(sourceNodeWithBlockAndDataOrder.data!![0], userWorkspaceID, Constants.SMART_CAPTURE_DEFAULT_NODE_ID, nodeWorkspaceMap.workspaceID, nodeWorkspaceMap.nodeID, dataOrder)
-        }
+        moveEntity(
+            request = request,
+            userID = userID,
+            userWorkspaceID = userWorkspaceID,
+            lambdaFunctionName = LambdaFunctionNames.CAPTURE_LAMBDA,
+            routePath = RoutePaths.CREATE_CAPTURE,
+            httpMethod = HttpMethods.POST,
+            defaultNodeID = Constants.SMART_CAPTURE_DEFAULT_NODE_ID,
+            entityID = captureID
+        )
     }
 
-    fun getAllSmartCapturesForFilter(workspaceID: String, userID: String, filterType: String, filterValue: String, lastKey: String?): MultipleEntityPaginatedResponse<SingleEntityResponse> {
-        return Helper.objectMapper.readValue(
-            invokeGetEntitiesWithFilterLambda(
-                        workspaceID,
-                        userID,
-                        filterType,
-                        filterValue,
-                        LambdaFunctionNames.CAPTURE_LAMBDA,
-                        RoutePaths.GET_ALL_CAPTURES_WITH_FILTER,
-                        HttpMethods.GET,
-                        lastKey), object : TypeReference<MultipleEntityPaginatedResponse<SingleEntityResponse>>() {})
+    fun getAllSmartCapturesForFilter(workspaceID: String, userID: String, filterType: String, filterValue: String, lastKey: String?): MultipleEntityPaginatedResponse<AdvancedElement> {
+        return getAllEntitiesForFilter(
+            userID = userID,
+            userWorkspaceID = workspaceID,
+            lambdaFunctionName = LambdaFunctionNames.CAPTURE_LAMBDA,
+            routePath = RoutePaths.GET_ALL_CAPTURES_WITH_FILTER,
+            httpMethod = HttpMethods.GET,
+            filterType = filterType,
+            filterValue = filterValue,
+            lastKey = lastKey
+        )
     }
 }
